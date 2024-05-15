6fa37ee87788d261f8c12a0fef2e993e5da5f5c8
SridharNaveenPA <160914542+SridharNaveenPA@users.noreply.github.com>
2024-05-15 10:10:18 +0530

    Add files via upload

---
+++ React/car.js
@@ -0,0 +1,15 @@
+export default function navbar(){
+  return (
+      <nav>
+      <div>
+          <img src="C:/Users/admin/my-react-app/src/pfp.jpeg" ></img>
+          <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Photos</li>
+              <li>Contact</li>
+          </ul>
+      </div>
+      </nav>
+  );
+}
---
+++ React/nav.js
@@ -0,0 +1,15 @@
+export default function navbar(){
+    return (
+        <nav>
+        <div>
+            <img src="pfp.jpeg" ></img>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Photos</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        </nav>
+    );
+}
